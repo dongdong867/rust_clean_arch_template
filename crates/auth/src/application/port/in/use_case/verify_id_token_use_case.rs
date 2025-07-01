297a30365de7eaf875f5b36d5d57368ee20117a4
dongdong867 <93483694+dongdong867@users.noreply.github.com>
2025-07-01 08:26:19 +0800

    feat: create use case inward port for application layer

---
+++ crates/auth/src/application/port/in/use_case/verify_id_token_use_case.rs
@@ -0,0 +1,11 @@
+use crate::application::error::auth_use_case_error::AuthUseCaseError;
+use crate::AuthenticatedUser;
+
+
+use async_trait::async_trait;
+
+
+#[async_trait]
+pub trait VerifyIdTokenUseCase: Send + Sync {
+    async fn execute(&self, id_token: &str) -> Result<AuthenticatedUser, AuthUseCaseError>;
+}
